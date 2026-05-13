a1229e853ef9262868e957846638cfbc771f8a2a
Ryan <chenghao55@gmail.com>
2026-05-13 16:12:24 +0800

    fix_module_info

--- space_ship/src/main/java/com/binge/GameProject/model/Player.java
+++ space_ship/src/main/java/com/binge/GameProject/model/Player.java
@@ -33,8 +33,8 @@ public class Player extends GameObject {
         this.gameManager = gm;
 
         // 嘗試使用 OBJImporter 載入戰鬥機模型
-        String modelPath = "src/main/java/com/binge/GameProject/resource/model/SpaceFighter.obj";
-        javafx.scene.shape.MeshView meshView = com.binge.GameProject.utils.OBJImporter.loadModel(modelPath);
+        //String modelPath = "src/main/java/com/binge/GameProject/resource/model/SpaceFighter.obj";
+        javafx.scene.shape.MeshView meshView = com.binge.GameProject.utils.OBJImporter.loadModel("SpaceFighter.obj");
 
         Group wrapper = new Group();
 
